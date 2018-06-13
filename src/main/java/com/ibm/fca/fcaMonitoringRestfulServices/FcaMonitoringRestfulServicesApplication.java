2f643be5c7c4bf1a4d6b648a2a873ef572f84bd5
Ray Shen <heinels1982@gmail.com>
2018-06-13 20:54:08 +0800

    7up

---
+++ src/main/java/com/ibm/fca/fcaMonitoringRestfulServices/FcaMonitoringRestfulServicesApplication.java
@@ -0,0 +1,12 @@
+package com.ibm.fca.fcaMonitoringRestfulServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcaMonitoringRestfulServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FcaMonitoringRestfulServicesApplication.class, args);
+	}
+}
